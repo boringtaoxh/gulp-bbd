00d2acf1c94de5c76023b87a830c4c1d14e0fc2c
boringtao <boringtaoxh@gmail.com>
2015-11-30 05:05:39 +0800

    test: directive beforeEach

--- build/test/public/directive.spec.coffee
+++ build/test/public/directive.spec.coffee
@@ -1,15 +1,20 @@
 'use strict'
 
 describe 'directive', ->
+  rootScope = scope = element = undefined
+
   beforeEach module('app')
 
-  it 'should assign scope emcee to element text when the onClick handler is called', inject ($rootScope, $compile) ->
+  beforeEach inject( ($rootScope, $compile) ->
     rootScope = $rootScope
     scope = $rootScope.$new()
     scope.emcee = 'Izzy Ice'
     element = angular.element('<emcee></emcee>')
     $compile(element)(scope)
     scope.$digest()
+  )
+
+  it 'should assign scope emcee to element text when the onClick handler is called', ->
     scope.onClick()
     expect(element.text()).to.be.equal 'Step up Izzy Ice!'
     return
--- test/public/directive.spec.js
+++ test/public/directive.spec.js
@@ -1,15 +1,18 @@
 'use strict';
 describe('directive', function() {
+  var element, rootScope, scope;
+  rootScope = scope = element = void 0;
   beforeEach(module('app'));
-  return it('should assign scope emcee to element text when the onClick handler is called', inject(function($rootScope, $compile) {
-    var element, rootScope, scope;
+  beforeEach(inject(function($rootScope, $compile) {
     rootScope = $rootScope;
     scope = $rootScope.$new();
     scope.emcee = 'Izzy Ice';
     element = angular.element('<emcee></emcee>');
     $compile(element)(scope);
-    scope.$digest();
+    return scope.$digest();
+  }));
+  return it('should assign scope emcee to element text when the onClick handler is called', function() {
     scope.onClick();
     expect(element.text()).to.be.equal('Step up Izzy Ice!');
-  }));
+  });
 });
